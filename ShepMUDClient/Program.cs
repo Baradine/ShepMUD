44ae8acec10a3595c2008ac4b58643e87daa3e93
Greg <beudaiu12@gmail.com>
2021-09-23 21:07:49 -0400

    Edited file structure for a client/server dual application.

---
+++ ShepMUDClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShepMUDClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
