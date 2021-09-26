289a09efdda6b01112b572f68239b3b529d44ca3
Greg <beudaiu12@gmail.com>
2021-09-26 08:04:08 -0400

    pull

---
+++ ShepMUDClient/Channel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShepMUDClient
+{
+    class Channel
+    {
+        public const int ADMIN = 0;
+        public const int GLOBAL = 1;
+        public const int MAX_LOG = 5000;
+
+        public int channelID { get; }
+
+        string[] messageLog;
+
+        // We might allow for persistant chat logs saved client side later, for now, restart log every time.
+        int currentIndex;
+
+        public Channel(int id)
+        {
+            this.channelID = id;
+            this.messageLog = new string[MAX_LOG];
+            this.currentIndex = 0;
+        }
+
+        public void AddMessage(string str)
+        {
+            messageLog[currentIndex] = str;
+            currentIndex++;
+            Chat.Update(channelID, str);
+        }
+    }
+}
--- ShepMUDClient/Chat.cs
+++ ShepMUDClient/Chat.cs
@@ -8,6 +8,7 @@ namespace ShepMUDClient
 {
     static class Chat
     {
+        public static List<Channel> channels = new List<Channel>();
         public static void HandleChat(Byte[] data, int index, int endex)
         {
             // Last 4 bytes detail  what channel it gets sent to.  We can use channels for private messages as well, simply by protecting the first
@@ -15,6 +16,35 @@ namespace ShepMUDClient
             // Given how that's over 4 Billion different channels, I highly doubt we'll ever reach a limit.
             string str = System.Text.Encoding.UTF8.GetString(data, index, endex - 4);
             int ch = BitConverter.ToInt32(data, endex - 3);
+
+            bool found = false;
+            foreach (Channel c in channels)
+            {
+                if (c.channelID == ch)
+                {
+                    found = true;
+                    c.AddMessage(str);
+                }
+            }
+            if (!found)
+            {
+                Channel c = new Channel(ch);
+                channels.Add(c);
+                c.AddMessage(str);
+            }
+        }
+
+        public static void Update(int id, string str)
+        {
+            // Once the GUI is up and running, we'd also have this add extra chat tabs
+            // If necessary.  For now, just outputs to console.
+            Console.WriteLine(str);
+        }
+
+        public static void InitGlobal()
+        {
+            Channel global = new Channel(Channel.GLOBAL);
+            channels[Channel.GLOBAL] = global;
         }
     }
 }
