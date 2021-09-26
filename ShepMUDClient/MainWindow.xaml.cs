107249185cceb4f3447da6cb0185547fa1cf10c4
Greg <beudaiu12@gmail.com>
2021-09-26 10:41:19 -0400

    basic foundation for the client game loop

--- ShepMUDClient/MainWindow.xaml.cs
+++ ShepMUDClient/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,16 +21,48 @@ namespace ShepMUDClient
     /// </summary>
     public partial class Window1 : Window
     {
-       // public TextBlock tb { set; get; }
+        bool test = false;
+        // public TextBlock tb { set; get; }
         public Window1()
         {
             InitializeComponent();
+            Title = "ShepMUD";
+            Width = 960;
+            Height = 540;
+            Show();
 
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+            //ExecutionLoop();
         }
 
-        public void changeText(string text)
+        public void ChangeText(string text)
         {
             ConsoleBox.Text = text;
+        }
+
+        
+
+        public void ExecutionLoop(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (test)
+            {
+                ChangeText("test");
+                test = false;
+            }
+            else if (!test)
+            {
+                ChangeText("blessed");
+                test = true;
+            }
+            
         }
 
     }
--- ShepMUDClient/Program.cs
+++ ShepMUDClient/Program.cs
@@ -2,58 +2,52 @@
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Threading;
+using System.Windows.Documents;
+using System.Windows.Media;
 
-namespace ShepMUDClient
+namespace ShepMUDClient 
 {
-    class Program
+    class Program : System.Windows.Application
     {
-
+        Window1 mainWindow = new Window1();
         [STAThread]
         static void Main(string[] args)
         {
-            string ip = "default";
-            int port = 25565;
+            Program prog = new Program();
+            prog.Run();
+            //string ip = "default";
+            //int port = 25565;
 
-            ConsoleHandler cs = new ConsoleHandler();
-
-            Window1 mainWindow = new Window1();
-            mainWindow.Title = "ShepMUD";
-            mainWindow.Width = 960;
-            mainWindow.Height = 540;
-            mainWindow.ShowDialog();
-
-
-            mainWindow.changeText("New text here");
-            //mainWindow.tb.Text = "Test\nfor\nfucks sake";
-            //mainWindow.tb.Inlines.Add("asdhjasghkdahsdsa");
+            //ConsoleHandler cs = new ConsoleHandler();
 
             // Console.Write("Input IP Address: ");
             //string ip = Console.ReadLine();
-            /*
-            if (ip == "default")
-            {
-                ip = "73.180.152.206";
-            }
-            else if(ip != "default" || ip == null)
-            {
+            //
+            //if (ip == "default")
+            //{
+            //    ip = "73.180.152.206";
+            //}
+            //else if(ip != "default" || ip == null)
+            //{
 
-               // Console.Write("Input Port: ");
+            //   // Console.Write("Input Port: ");
 
-                try
-                {
-                    //port = int.Parse(Console.ReadLine());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("There was an issue with the port");
-                }
-            }
+            //    try
+            //    {
+            //        //port = int.Parse(Console.ReadLine());
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine("There was an issue with the port");
+            //    }
+            //}
 
 
-            NetworkConnection multiClient = new NetworkConnection(port, IPAddress.Parse(ip));
-            multiClient.Connect();
-            //multiClient.TransmitToServer("A new client has appeared.");
-            */
+            //NetworkConnection multiClient = new NetworkConnection(port, IPAddress.Parse(ip));
+            //multiClient.Connect();
+            ////multiClient.TransmitToServer("A new client has appeared.");
+            //Console.Read();
         }
     }
 }
