c980df32995cc3be55021c6e6a98c4695f8e47c9
Greg <beudaiu12@gmail.com>
2021-09-28 16:31:18 -0400

    minor bugfix, removing the .suo files as they were annoying and not necessary

--- ShepMUDClient/MainWindow.xaml.cs
+++ ShepMUDClient/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace ShepMUDClient
 {
@@ -64,53 +65,15 @@ namespace ShepMUDClient
         {
             if (sender == M1)                       //Menu1 pressed
             {
-                Console.WriteLine("M1 Pressed");                    //<---- debugging behaviour. Change before deployment.
+                Trace.WriteLine("M1 Pressed");                    //<---- debugging behaviour. Change before deployment.
             }
             else if (sender == M2)                  //Menu2 pressed
             {
-                Console.WriteLine("M2 Pressed");
+                Trace.WriteLine("M2 Pressed");
             }
             else if (sender == M3)                 //Menu3 pressed
             {
-                Console.WriteLine("M3 Pressed");
-            }
-        }
-
-        /**
-         *  Actionhandler which displays the menu items
-         **/
-        private void menuDropDown_Click(object sender, RoutedEventArgs e)
-        {
-            if(menuVis == false)    //if hidden
-            {
-                M1.Visibility = Visibility.Visible; //make not hidden
-                M2.Visibility = Visibility.Visible;
-                M3.Visibility = Visibility.Visible;
-            } else          //else
-            {
-                M1.Visibility = Visibility.Hidden;  //make hidden
-                M2.Visibility = Visibility.Hidden;
-                M3.Visibility = Visibility.Hidden;
-            }
-            menuVis = !menuVis;
-        }
-        
-        /**
-         *  Action Handler for Menu
-         **/
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender == M1)                       //Menu1 pressed
-            {
-                Console.WriteLine("M1 Pressed");                    //<---- debugging behaviour. Change before deployment.
-            }
-            else if (sender == M2)                  //Menu2 pressed
-            {
-                Console.WriteLine("M2 Pressed");
-            }
-            else if (sender == M3)                 //Menu3 pressed
-            {
-                Console.WriteLine("M3 Pressed");
+                Trace.WriteLine("M3 Pressed");
             }
         }
 
